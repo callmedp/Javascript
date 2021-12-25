375e18ba0f5a59f92f9c0d2e17ff02fc7b68abbf
Carat^ <divyanshu3490@gmail.com>
2021-12-25 11:37:56 +0530

    objects file updated

--- javascriptInfo/Function/object.js
+++ javascriptInfo/Function/object.js
@@ -10,7 +10,11 @@ console.log("in the madness of ", arr[0].name)
 //Function is a special object
 //callable object
 
-function hello(){
+function hello(a, b,...c){
 }
 
+//Accessing name property
 console.log("hello", hello.name)
+
+//counting number of parameters in function
+console.log("paramsF(...args)", hello.length)
